10317b09c98005de045c7cb021177df36b67e9d9
SkyLord2 <chengdongsheng@outlook.com>
2026-08-14 17:01:15 +0800

    add(pub):自定义发布配置

--- learning_topic/src/node_pub.rs
+++ learning_topic/src/node_pub.rs
@@ -1,8 +1,10 @@
 use std::time::Duration;
+use std::sync::atomic::{AtomicUsize, Ordering};
 use anyhow::Result;
-use rclrs::{Context, CreateBasicExecutor, RclrsErrorFilter, SpinOptions, log_info};
+use rclrs::{Context, CreateBasicExecutor, RclrsErrorFilter, SpinOptions, PublisherOptions, QoSProfile, QoSHistoryPolicy, log_info};
 use std_msgs::msg::String as StringMsg;
 
+static PUBLISH_COUNT: AtomicUsize = AtomicUsize::new(0);
 /// Creates a ROS 2 context and node, prints a hello message,
 /// then spins until shutdown.
 fn main() -> Result<()> {
@@ -11,11 +13,18 @@ fn main() -> Result<()> {
     let node = executor.create_node("node_pub")?;
     let logger = node.logger();
     log_info!(logger, "ROS2节点示例：发布节点");
-    let publisher = node.create_publisher::<StringMsg>("hello_world")?;
-    let options = rclrs::TimerOptions::new(Duration::from_secs(1));
-    let _timer = node.create_timer_repeating(options, move || {
+    
+    let mut publisher_options = PublisherOptions::from("hello_world");
+    let mut qos = QoSProfile::default();
+    qos.history = QoSHistoryPolicy::KeepLast { depth: 20 };
+    publisher_options.qos = qos;
+
+    let publisher = node.create_publisher::<StringMsg>(publisher_options)?;
+    let timer_options = rclrs::TimerOptions::new(Duration::from_secs(1));
+    let _timer = node.create_timer_repeating(timer_options, move || {
+        let old_val = PUBLISH_COUNT.fetch_add(1, Ordering::Relaxed);
         let msg = StringMsg {
-            data: "Hello, ROS2 from Rust!".to_string(),
+            data: format!("Hello, Count from ros2 pub node: {}", old_val + 1),
         };
         publisher.publish(msg).unwrap();
     })?;
