64a8f9e47ba23f5617a37565bcb06192e89210fb
SkyLord2 <chengdongsheng@outlook.com>
2026-08-27 16:00:11 +0800

    add(sub_novel_node): 实现小说内容的异步处理和输出功能

--- learning_topic_cpp/src/sub_novel_node.cpp
+++ learning_topic_cpp/src/sub_novel_node.cpp
@@ -1,10 +1,20 @@
 #include <rclcpp/rclcpp.hpp>
 #include <std_msgs/msg/string.hpp>
+#include <thread>
+#include <mutex>
+#include <queue>
+#include <condition_variable>
+#include <atomic>
 
 class SubNovelNode : public rclcpp::Node
 {
 private:
     rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
+    std::queue<std::string> content_queue_;
+    std::thread speaking_thread_;
+    std::mutex queue_mutex_;
+    std::condition_variable cv_;
+    std::atomic<bool> is_running_{true};
 private:
 
 public:
@@ -14,12 +24,52 @@ public:
             "novel_content", 
             qos, 
             [this](const std_msgs::msg::String::SharedPtr msg) {
-                RCLCPP_INFO(this->get_logger(), "小说内容：%s", msg->data.c_str());
+                {
+                    std::lock_guard<std::mutex> lock(queue_mutex_);
+                    content_queue_.push(msg->data);
+                }
+                cv_.notify_one();
             }
         );
+        speaking_thread_ = std::thread([this]() {
+            while (is_running_ && rclcpp::ok())
+            {
+                std::string content;
+                {
+                    std::unique_lock<std::mutex> lock(queue_mutex_);
+                    cv_.wait(lock, [this]() {
+                        return !content_queue_.empty() || !is_running_ || !rclcpp::ok();
+                    });
+
+                    if (!is_running_ || !rclcpp::ok())
+                    {
+                        break;
+                    }
+                    
+
+                    if (!content_queue_.empty())
+                    {
+                        content = content_queue_.front();
+                        content_queue_.pop();    
+                    }
+                }
+                
+                if (!content.empty())
+                {
+                    RCLCPP_INFO(this->get_logger(), "小说内容：%s", content.c_str());
+                }
+                
+            }
+        });
     }
     ~SubNovelNode() {
-
+        is_running_ = false;
+        cv_.notify_all();
+        if (speaking_thread_.joinable())
+        {
+            speaking_thread_.join();
+        }
+        
     }
 };
 
