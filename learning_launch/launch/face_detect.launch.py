dd723aeb9979ec9c74dfb6f95c107568b8332913
SkyLord2 <chengdongsheng@outlook.com>
2026-08-31 18:05:19 +0800

    add(face_detect.launch): 添加模型路径参数以支持动态配置

--- learning_launch/launch/face_detect.launch.py
+++ learning_launch/launch/face_detect.launch.py
@@ -3,17 +3,26 @@ import launch_ros
 
 def generate_launch_description():
 
+    launch_model_path = launch.actions.DeclareLaunchArgument(
+        name="launch_model_path",
+        default_value="/home/cds/Code/ros2/dev_ws/src/ros2_rust_tutorials/face_detection_yunet_2022mar.onnx",
+        description="Path to YuNet ONNX model file"
+    )
     face_detect_client = launch_ros.actions.Node(
         package="face_detect_cpp",
-        executable="face_detect_client"
+        executable="face_detect_client",
+        output='screen'
     )
 
     face_detect_server = launch_ros.actions.Node(
         package="face_detect_cpp",
-        executable="face_detect_server"
+        executable="face_detect_server",
+        parameters=[{'model_path': launch.substitutions.LaunchConfiguration('launch_model_path')}],
+        output='screen'
     )
 
     return launch.LaunchDescription([
+        launch_model_path,
         face_detect_client,
         face_detect_server
     ])
