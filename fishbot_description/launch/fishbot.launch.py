49381c28eaccb0ae6110b04f7716c62337d029a1
SkyLord2 <chengdongsheng@outlook.com>
2026-09-03 09:56:29 +0800

    add(fishbot_description): 添加机器人XACRO描述文件及相关传感器配置，包括IMU、相机和激光传感器

--- fishbot_description/launch/fishbot.launch.py
+++ fishbot_description/launch/fishbot.launch.py
@@ -5,7 +5,7 @@ import os
 
 def generate_launch_description():
     urdf_pkg_path = get_package_share_directory('fishbot_description')
-    default_urdf_path = os.path.join(urdf_pkg_path, 'urdf', 'first_robot.urdf')
+    default_urdf_path = os.path.join(urdf_pkg_path, 'urdf', 'fishbot', 'fishbot.urdf.xacro')
     default_rviz_path = os.path.join(urdf_pkg_path, 'config', 'display_robot_model.rviz')
 
     action_declare_arg_mode_path = launch.actions.DeclareLaunchArgument(
@@ -14,7 +14,7 @@ def generate_launch_description():
         description='Path to robot urdf file'
     )
 
-    substitutions_command_result = launch.substitutions.Command(['cat ', launch.substitutions.LaunchConfiguration('model')])
+    substitutions_command_result = launch.substitutions.Command(['xacro ', launch.substitutions.LaunchConfiguration('model')])
 
     robot_description_value = launch_ros.parameter_descriptions.ParameterValue(
         substitutions_command_result,
@@ -24,18 +24,21 @@ def generate_launch_description():
     action_robot_state_publisher = launch_ros.actions.Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
+        output='screen',
         parameters=[{'robot_description': robot_description_value}]
     )
 
     action_joint_state_publisher = launch_ros.actions.Node(
         package='joint_state_publisher',
-        executable='joint_state_publisher'
+        executable='joint_state_publisher',
+        output='screen',
     )
 
     action_rviz_node = launch_ros.actions.Node(
         package='rviz2',
         executable='rviz2',
-        arguments=['-d', default_rviz_path]
+        arguments=['-d', default_rviz_path],
+        output='screen',
     )
 
     return launch.LaunchDescription([
