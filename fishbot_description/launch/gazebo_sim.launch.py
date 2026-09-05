c906a43af2f0e4f4d37b13c4f1ce90768b882cd8
SkyLord2 <chengdongsheng@outlook.com>
2026-09-05 21:59:04 +0800

    add(fishbot_description): 添加差速驱动控制器配置，更新控制器加载逻辑和URDF文件中的重映射

--- fishbot_description/launch/gazebo_sim.launch.py
+++ fishbot_description/launch/gazebo_sim.launch.py
@@ -66,8 +66,12 @@ def generate_launch_description():
     )
 
     # 加载并激活 fishbot_effort_controller 控制器
-    load_fishbot_effort_controller = launch.actions.ExecuteProcess(
-        cmd=['ros2', 'control', 'load_controller', '--set-state', 'active','fishbot_effort_controller'], 
+    # load_fishbot_effort_controller = launch.actions.ExecuteProcess(
+    #     cmd=['ros2', 'control', 'load_controller', '--set-state', 'active','fishbot_effort_controller'], 
+    #     output='screen')
+
+    load_fishbot_diff_drive_controller = launch.actions.ExecuteProcess(
+        cmd=['ros2', 'control', 'load_controller', '--set-state', 'active','fishbot_diff_drive_controller'], 
         output='screen')
 
     return launch.LaunchDescription([
@@ -87,6 +91,6 @@ def generate_launch_description():
         launch.actions.RegisterEventHandler(
             event_handler=launch.event_handlers.OnProcessExit(
             target_action=load_joint_state_controller,
-            on_exit=[load_fishbot_effort_controller],)
+            on_exit=[load_fishbot_diff_drive_controller],)
         ),
     ])
