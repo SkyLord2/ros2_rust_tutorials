abd037059ce50759435a7303802394fcd772d3e8
SkyLord2 <chengdongsheng@outlook.com>
2026-09-07 16:41:46 +0800

    add(fishbot_application): 初始化机器人初始位姿

---
+++ fishbot_application/fishbot_application/init_robot_pose.py
@@ -0,0 +1,19 @@
+from geometry_msgs.msg import PoseStamped
+from nav2_simple_commander.robot_navigator import BasicNavigator
+import rclpy
+
+def main():
+    rclpy.init()
+    navigator = BasicNavigator()
+    init_pose = PoseStamped()
+    init_pose.header.frame_id = 'map'
+    init_pose.header.stamp = navigator.get_clock().now().to_msg()
+    init_pose.pose.position.x = 0.0
+    init_pose.pose.position.y = 0.0
+    init_pose.pose.orientation.w = 1.0
+    navigator.setInitialPose(init_pose)
+    # 等待导航可用
+    navigator.waitUntilNav2Active()
+
+    rclpy.spin(navigator)
+    rclpy.shutdown()
---
+++ fishbot_application/setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+
+package_name = 'fishbot_application'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='cds',
+    maintainer_email='chengdongsheng@outlook.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'init_robot_pose=fishbot_application.init_robot_pose:main',
+        ],
+    },
+)
